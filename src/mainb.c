77ff45ed5a9c06469672836d1e8ee9c6b3e40620
Lucas Emanoell Medeiros e Silva <lucas.emanoell@outlook.com>
2019-06-10 14:12:52 -0300

    Update ~> menu.c & mainb.c

--- src/mainb.c
+++ src/mainb.c
@@ -1,20 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include "read.h"
+#include "../include/read.h"
 #include "../include/menu.h"
 #include "../include/write.h"
 #include "../include/util.h"
 
 int main()
 {
-	char *filepath;
-
-	Image originalImg;
-
-	originalImg = readPPM(filepath);
-
-	//printMenu();
-	//menuOptions();
+	printMenu();
+	menuOptions();
 
 	return 0;
 }
