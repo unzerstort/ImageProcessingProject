fa5983aa4c1caae6b69b8339aba4d80bd4e22702
Sandra Bastos <sandra@vermillion.net.br>
2019-06-17 20:01:40 -0300

    merged both write.c and read.c into file.c

--- src/file.c
+++ src/file.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include "read.h"
+#include "file.h"
 
 Image readPPM(char *filepath)
 {
@@ -14,7 +14,7 @@ Image readPPM(char *filepath)
 	image = fopen(filepath, "r");
 
 	printf("Reading image file...\n");
-	
+
 	if (image == NULL)
 	{
 		printf("File doesn't exist!\n");
@@ -24,7 +24,7 @@ Image readPPM(char *filepath)
 	{
 		printf("File loaded successfully :)\n");
 	}
-	
+
 	Image img;
 	char format[10];
 
@@ -52,7 +52,7 @@ Image readPPM(char *filepath)
 	{
 		printf("Dimensions (Width x Height): %u x %u pixels.\n", img.width, img.height);
 	}
-	
+
 	// reading maximum RGB value
 	if (fscanf(image, "%u", &img.maxRGB) != 1)
 	{
@@ -63,9 +63,9 @@ Image readPPM(char *filepath)
 		printf("The maximum RGB value is: %u.\n", img.maxRGB);
 	}
 
-	// pixel mapping 
+	// pixel mapping
 	img.pixels = (Pixel**) calloc(img.height, sizeof(Pixel*));
-	
+
 	int loadedPix = 0;
 	for (int i = 0; i < img.height; i++)
 	{
@@ -74,7 +74,7 @@ Image readPPM(char *filepath)
 		{
 			fscanf(image, "%hhu\n%hhu\n%hhu\n", &img.pixels[i][j].red, &img.pixels[i][j].green, &img.pixels[i][j].blue);
 			++loadedPix;
-			
+
 			//uncomment the next line if you want to see the actual pixels
 			//printf("rgb(%hhu, %hhu, %hhu)\n", img.pixels[i][j].red, img.pixels[i][j].green, img.pixels[i][j].blue);
 		}
@@ -83,4 +83,35 @@ Image readPPM(char *filepath)
 	printf("Loaded pixels: %d\n", loadedPix);
 
 	return img;
+}
+
+void writePPM(char *filepath, Image img)
+{
+	filepath = calloc(100, sizeof(char));
+
+	printf("Insert the file's destination, as well as its new name in the end.\n For instance: /home/user/ImageProcessing-ITP/newfile.ppm.\n");
+	scanf("%s", filepath);
+
+	FILE *outImage;
+	outImage = fopen(filepath, "w");
+
+	//writing the image's header
+	fprintf(outImage, "P3\n");
+	fprintf(outImage, "%u %u\n", img.width, img.height);
+	fprintf(outImage, "%u\n", img.maxRGB);
+
+	//writing all rgb values
+	int i, j;
+
+	for (i = 0; i < img.height; i++)
+	{
+		for (j = 0; j < img.width; j++)
+		{
+			fprintf(outImage, "%i\n%i\n%i\n", img.pixels[i][j].red, img.pixels[i][j].green, img.pixels[i][j].blue);
+		}
+	}
+
+	fclose(outImage);
+
+	return;
 }
