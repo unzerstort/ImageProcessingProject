2fc000dae4fce6c6eac4ea99ac884ee191ab650c
Sandra Bastos <sandra@vermillion.net.br>
2019-06-18 15:06:08 -0300

    added library of transformation functions

---
+++ include/transform.h
@@ -0,0 +1,19 @@
+#ifndef TRANSFORM_HEADER
+#define TRANSFORM_HEADER
+
+/* ___________________________
+ *|                           |
+ *|  this library has all the |
+ *|  signatures of the        |
+ *|  functions related to     |
+ *|  rotation and zoom!       |
+ *|___________________________|
+*/
+  
+
+#include "util.h"
+
+Image rotate90DegreesRight(Image);
+
+
+#endif
