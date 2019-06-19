59cc3a86f748de3e0062e12eded1cdc34c965dd6
Lucas Emanoell Medeiros e Silva <lucas.emanoell@outlook.com>
2019-06-19 18:20:41 -0300

    new function: zoom in ~> transform.c

--- src/transform.c
+++ src/transform.c
@@ -59,7 +59,7 @@ Image rotate90DegreesRight(Image img)
 	return rotated;
 }
 
-Image zoomOut(Image img)
+Image zoomIn(Image img)
 {
 	int i, j;
 	Image zoom;
@@ -67,7 +67,7 @@ Image zoomOut(Image img)
 	zoom.height = img.height * 2;
 	zoom.width = img.width * 2;
 	zoom.maxRGB = img.maxRGB;
-	printf("heightxwidth: %d X %d", zoom.height, zoom.width);	
+		
 	zoom.pixels = (Pixel**) calloc(zoom.height,  sizeof(Pixel*));
 	for(i = 0; i < zoom.height; i++)
 	{
@@ -89,36 +89,60 @@ Image zoomOut(Image img)
 			{
 				if(j%2 ==0)
 				{	
-					/*if(i%2 == 0)
-					{*/
 					zoom.pixels[i][j].red = img.pixels[aux][j/2].red;
 					zoom.pixels[i][j].green = img.pixels[aux][j/2].green;
 					zoom.pixels[i][j].blue = img.pixels[aux][j/2].blue;
-					/*}
-					else
-					{
-					zoom.pixels[i][j].red = img.pixels[i-1][j/2].red;
-					zoom.pixels[i][j].green = img.pixels[i-1][j/2].green;
-					zoom.pixels[i][j].blue = img.pixels[i-1][j/2].blue;
-					}*/
 				}
 				else
 				{
-					/*if(i%2 == 0)
-					{*/
 					zoom.pixels[i][j].red = img.pixels[aux][(j-1)/2].red;
 					zoom.pixels[i][j].green = img.pixels[aux][(j-1)/2].green;
 					zoom.pixels[i][j].blue = img.pixels[aux][(j-1)/2].blue;
-					/*}
-					else
-					{
-					zoom.pixels[i][j].red = img.pixels[i-1][j-1/2].red;
-					zoom.pixels[i][j].green = img.pixels[i-1][j-1/2].green;
-					zoom.pixels[i][j].blue = img.pixels[i-1][j-1/2].blue;
-					}*/
 				}
 			}
 		}
 	
 	return zoom;
+}
+Image zoomOut(Image img)
+{	
+	int i, j;
+	Image zoom;
+	if(img.height % 2 == 0)
+	{
+		zoom.height = img.height / 2;	
+	}
+	else
+	{
+		zoom.height = (img.height - 1) / 2;	
+	}
+	if(img.width % 2 == 0)
+	{
+		zoom.width = img.width / 2;	
+	}
+	else
+	{
+		zoom.width = (img.width - 1) / 2;	
+	}
+	zoom.maxRGB = img.maxRGB;
+	printf("Tamanho: %d x %d\n", zoom.height, zoom.width);
+	zoom.pixels = (Pixel**) calloc(zoom.height,  sizeof(Pixel*));
+	for(i = 0; i < zoom.height; i++)
+	{
+		zoom.pixels[i] = (Pixel*) calloc(zoom.width, sizeof(Pixel));
+	}
+	int aux1=0, aux2=0;
+	for(i = 0; i < img.height; i+=2)
+	{
+		for(j = 0; j < img.width; j+=2)
+		{
+			zoom.pixels[aux1][aux2].red = ((img.pixels[i][j].red + img.pixels[i+1][j].red + img.pixels[i+1][j+1].red + img.pixels[i][j+1].red)/4);
+			zoom.pixels[aux1][aux2].green = ((img.pixels[i][j].green + img.pixels[i+1][j].green + img.pixels[i+1][j+1].green + img.pixels[i][j+1].green)/4);
+			zoom.pixels[aux1][aux2].blue = ((img.pixels[i][j].blue + img.pixels[i+1][j].blue + img.pixels[i+1][j+1].blue + img.pixels[i][j+1].blue)/4);
+			aux2++;
+		}
+		aux2 = 0;
+		aux1++;
+	}
+	return zoom;	
 }
